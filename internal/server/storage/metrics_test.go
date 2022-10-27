df4c5bfa701020593625ed228bbe9d66355c40f3
Timur Idiyatullin <easservice@gmail.com>
2022-10-27 22:46:37 +0300

    metrics test refactoring

--- internal/server/storage/metrics_test.go
+++ internal/server/storage/metrics_test.go
@@ -1,56 +1,57 @@
 package storage
 
 import (
-	"github.com/atrian/devmetrics/internal/appconfig/serverconfig"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/atrian/devmetrics/internal/appconfig/serverconfig"
 )
 
-func TestStorage_StoreCounter(t *testing.T) {
-	config := serverconfig.NewServerConfig()
-	storage := NewMemoryStorage(config)
+type HandlersTestSuite struct {
+	suite.Suite
+	config  *serverconfig.Config
+	storage Repository
+}
 
-	storage.StoreCounter("PollCount", int64(1))
+func (suite *HandlersTestSuite) SetupSuite() {
+	suite.config = serverconfig.NewServerConfig()
+}
 
-	// значение сохранилось в мапе
-	assert.Equal(t, counter(1), storage.metrics.CounterDict["PollCount"])
+func (suite *HandlersTestSuite) SetupTest() {
+	suite.storage = NewMemoryStorage(suite.config)
+}
 
-	storage.StoreCounter("PollCount", int64(2022))
+func (suite *HandlersTestSuite) TestStorage_StoreCounter() {
+	suite.storage.StoreCounter("PollCount", int64(1))
+
+	value, exist := suite.storage.GetCounter("PollCount")
+	assert.Equal(suite.T(), true, exist)
+	assert.Equal(suite.T(), int64(1), value)
+
+	suite.storage.StoreCounter("PollCount", int64(2022))
 	// при последующем сохранении значение увеличилось а не перезаписалось 2022 + 1
-	assert.Equal(t, counter(2023), storage.metrics.CounterDict["PollCount"])
+	value, _ = suite.storage.GetCounter("PollCount")
+	assert.Equal(suite.T(), int64(2023), value)
 }
 
-func TestStorage_GetCounter(t *testing.T) {
-	config := serverconfig.NewServerConfig()
-	storage := NewMemoryStorage(config)
-	storage.StoreCounter("PollCount", 1585)
-
-	val, exist := storage.GetCounter("PollCount")
-	assert.Equal(t, true, exist)
-	assert.Equal(t, int64(1585), val) // значение можно получить через метод и оно совпадает с ожидаемым
-}
-
-func TestStorage_StoreGauge(t *testing.T) {
-	config := serverconfig.NewServerConfig()
-	storage := NewMemoryStorage(config)
-
-	storage.StoreGauge("Alloc", float64(1))
+func (suite *HandlersTestSuite) TestStorage_StoreGauge() {
+	suite.storage.StoreGauge("Alloc", float64(1))
 
 	// значение сохранилось в мапе по ключу Alloc
-	assert.Equal(t, gauge(1), storage.metrics.GaugeDict["Alloc"])
+	val, exist := suite.storage.GetGauge("Alloc")
+	assert.Equal(suite.T(), true, exist)
+	assert.Equal(suite.T(), float64(1), val)
 
-	storage.StoreGauge("Alloc", float64(777))
+	suite.storage.StoreGauge("Alloc", float64(777))
+
 	// при последующем сохранении значение перезаписалось
-	assert.Equal(t, gauge(777), storage.metrics.GaugeDict["Alloc"])
+	val, _ = suite.storage.GetGauge("Alloc")
+	assert.Equal(suite.T(), float64(777), val)
 }
 
-func TestStorage_GetGauge(t *testing.T) {
-	config := serverconfig.NewServerConfig()
-	storage := NewMemoryStorage(config)
-	storage.StoreGauge("Alloc", float64(777))
-
-	val, exist := storage.GetGauge("Alloc")
-	assert.Equal(t, true, exist)
-	assert.Equal(t, float64(777), val)
+// Для запуска через Go test
+func TestHandlersTestSuite(t *testing.T) {
+	suite.Run(t, new(HandlersTestSuite))
 }
