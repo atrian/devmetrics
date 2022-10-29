f2b470872bef53675a84c8bd2f14bb854b2f093a
Timur Idiyatullin <easservice@gmail.com>
2022-10-29 23:28:15 +0300

    migration INT to BIGINT type changed for delta

--- internal/server/database/migrations/000001_init.up.sql
+++ internal/server/database/migrations/000001_init.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS public.metrics
 (
     id VARCHAR PRIMARY KEY,
     type VARCHAR not null,
-    delta INT null,
+    delta BIGINT null,
     value DOUBLE PRECISION null
 );
